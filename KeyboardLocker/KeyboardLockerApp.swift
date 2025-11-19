b0604fe49e6b436f920c1bc4958a6ffd9422bd6a
Eden <levine5blue@gmail.com>
2025-11-19 15:55:54 +0800

    ✨ feat: initialize project structure and add assets.

---
+++ KeyboardLocker/KeyboardLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardLockerApp.swift
+//  KeyboardLocker
+//
+//  Created by Eden on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardLockerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
